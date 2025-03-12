bd176e4996c67642ea0c34bfff2938af38900a38
Xianghua Li <37569717+sunflowerlily@users.noreply.github.com>
2025-03-12 11:59:17 +0800

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,28 +1,16 @@
-
 def is_prime(n):
     """
     检验一个数是否是质数
-    
-    参数:
-    n (int): 需要检验的整数
-    
-    返回:
-    bool: 如果n是质数返回True，否则返回False
+    :param n: 要检验的数
+    :return: 如果是质数返回True，否则返回False
     """
-    # 处理特殊情况
     if n <= 1:
         return False
-    if n <= 3:
+    if n == 2:
         return True
-    if n % 2 == 0 or n % 3 == 0:
+    if n % 2 == 0:
         return False
-    
-    # 使用6k±1优化的试除法
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
             return False
-        i += 6
-    
     return True
-
